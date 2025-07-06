71997c642a84b215f9e138fb58a750b7f63775f8
Aashish Kumar Jha <ash1999.official@gmail.com>
2025-07-06 16:06:52 +0530

    new code

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String str = null;
-        System.out.println(str.length());
+        System.out.println(str.toLowerCase());
         int x = 5/0;
 
         //This code is on github and has two issues! -> Debugger takes the repo url as input
