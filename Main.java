09dee8333ef9434d0100f7108014d4204be71132
Aashish Kumar Jha <ash1999.official@gmail.com>
2025-07-06 16:05:39 +0530

    new code

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         String str = null;
-        System.out.println(str.length()); // Deliberate NPE
-        System.out.println(5/0);
+        System.out.println(str.length());
+        int x = 5/0;
+
+        //This code is on github and has two issues! -> Debugger takes the repo url as input
+        //Ollama runs in my local PC
+
     }
 }
